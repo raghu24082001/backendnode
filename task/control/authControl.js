9e15fc5ce87072f97729b15dfb08868b6dc6e068
raghu24082001 <raghudae248@gmail.com>
2026-04-18 13:05:33 +0530

    new data

--- task/control/authControl.js
+++ task/control/authControl.js
@@ -18,3 +18,23 @@ export const createdata = async (req,res)=>{
 
       }
 }
+
+export const getdata = async (req,res)=>{
+
+      try {
+       
+       
+
+       const createdatabase = await newdatamodal.find()
+
+       res.status(200).json(createdatabase)
+
+       console.log(createdatabase)
+
+      } catch (error) {
+
+         res.status(500).json({msg:"error",error})
+          console.log('error',error)
+
+      }
+}
--- task/router/authRoute.js
+++ task/router/authRoute.js
@@ -1,10 +1,14 @@
 import express, { Router } from "express"
-import { createdata } from "../control/authControl.js"
+import { createdata, getdata } from "../control/authControl.js"
 
 const route = Router()
 
 route.post("/create",createdata)
 
+route.get("/getdata",getdata)
+
 //http://localhost:5000/api/user/create
+
+//http://localhost:5000/api/user/getdata
 
 export default route
